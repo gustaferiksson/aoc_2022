df7f8132cd1dca9a1c1cebcf8b20449002ff989b
Gustaf <gustaf.eriksson@cygni.se>
2022-12-01 19:54:15 +0100

    Better variable names, Object literals for parts

--- day01/index.ts
+++ day01/index.ts
@@ -1,17 +1,22 @@
 import { file } from 'bun';
 const input = await file('input.txt').text();
 
+const add = (a: number, b: number | string) => a + Number(b);
+const reverse = (a: number, b: number) => b - a;
+
 const solution = (input: string, top = 1): number => {
   return input
     .split('\n\n')
-    .map(x => x.split('\n'))
-    .map(x => x.reduce((a, b) => a + Number(b), 0))
-    .sort((a, b) => b - a)
+    .map(elf => elf.split('\n'))
+    .map(calories => calories.reduce(add, 0))
+    .sort(reverse)
     .splice(0, top)
-    .reduce((a, b) => a + b);
+    .reduce(add, 0);
 };
 
-export const part1 = (input: string) => solution(input, 1);
-export const part2 = (input: string) => solution(input, 3);
+const part = {
+  part1: () => solution(input),
+  part2: () => solution(input, 3),
+};
 
-console.log(process.env.part === 'part1' ? part1(input) : part2(input));
+console.log(part[process.env.part]());
