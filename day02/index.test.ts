4ac31afdf4d7153f909bb5cb787117a4f621dd78
Gustaf <gustaf.eriksson@cygni.se>
2022-12-02 09:00:54 +0100

    Day 02!

---
+++ day02/index.test.ts
@@ -0,0 +1,23 @@
+import { file } from 'bun';
+import { describe, expect, it } from 'bun:test';
+import { input, day02 } from '.';
+
+const example = await file('example.txt').text();
+
+describe('Day 02', () => {
+  it('Part 1 - Example', () => {
+    expect(day02.part1(example)).toBe(15);
+  });
+
+  it('Part 2 - Example', () => {
+    expect(day02.part2(example)).toBe(12);
+  });
+
+  it('Part 1 - Input', () => {
+    expect(day02.part1(input)).toBe(11150);
+  });
+
+  it('Part 2 - Input', () => {
+    expect(day02.part2(input)).toBe(8295);
+  });
+});
---
+++ day02/index.ts
@@ -0,0 +1,48 @@
+import { file } from 'bun';
+export const input = await file('input.txt').text();
+
+const scoreMap = {
+  'A X': 4,
+  'A Y': 8,
+  'A Z': 3,
+  'B X': 1,
+  'B Y': 5,
+  'B Z': 9,
+  'C X': 7,
+  'C Y': 2,
+  'C Z': 6,
+};
+
+// rock paper scissors
+const translatedScoreMap = {
+  'A X': 'A Z', // lose
+  'A Y': 'A X', // draw
+  'A Z': 'A Y', // win
+  'B X': 'B X', // lose
+  'B Y': 'B Y', // draw
+  'B Z': 'B Z', // win
+  'C X': 'C Y', // lose
+  'C Y': 'C Z', // draw
+  'C Z': 'C X', // win
+};
+
+const solution1 = (input: string): number => {
+  return input
+    .split('\n')
+    .map(turn => scoreMap[turn])
+    .reduce((a, b) => a + b);
+};
+const solution2 = (input: string): number => {
+  return input
+    .split('\n')
+    .map(turn => scoreMap[translatedScoreMap[turn]])
+    .reduce((a, b) => a + b);
+};
+
+export const day02 = {
+  part1: (input: string) => solution1(input),
+  part2: (input: string) => solution2(input),
+  default: () => 'env variable part is missing',
+};
+
+console.log(day02[process.env.part || 'default'](input));
