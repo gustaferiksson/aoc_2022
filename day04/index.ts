c49487ae9c3feb899a0f54b42c69d0c0fbf9f148
Gustaf <gustaf.eriksson@cygni.se>
2022-12-04 14:28:51 +0100

    A little nicer to read

--- day04/index.ts
+++ day04/index.ts
@@ -1,31 +1,39 @@
 import { file } from 'bun';
-export const input = await file('input.txt').text();
+export const input = await file('example.txt').text();
 
 const add = (a: number, b: number) => a + b;
 
-type Range = [string, string];
+type Range = { from: number; to: number };
 
-const fullOverlap = ([from1, to1]: Range, [from2, to2]: Range) => {
-  return (+from1 >= +from2 && +to1 <= +to2) || (+from2 >= +from1 && +to2 <= +to1) ? 1 : 0;
+const fullOverlap = (a: Range, b: Range) => {
+  const c1 = a.from >= b.from && a.to <= b.to;
+  const c2 = b.from >= a.from && b.to <= a.to;
+  return c1 || c2 ? 1 : 0;
 };
 
-const partialOverlap = ([from1, to1]: Range, [from2, to2]: Range) => {
-  return (+from1 >= +from2 && +from1 <= +to2) || (+from2 >= +from1 && +from2 <= +to1) ? 1 : 0;
+const partialOverlap = (a: Range, b: Range) => {
+  const c1 = a.from >= b.from && a.from <= b.to;
+  const c2 = b.from >= a.from && b.from <= a.to;
+  return c1 || c2 ? 1 : 0;
 };
 
-const solution = (input: string, comparator: Function): number => {
+const toRange = (s: string) => {
+  const [from, to] = s.split('-').map(Number);
+  return { from, to };
+};
+
+const solution = (input: string, comparator: Function): any => {
   return input
     .split('\n')
     .map(row => row.split(','))
-    .map(pair => pair.map(range => range.split('-')))
-    .map(([range1, range2]) => comparator(range1, range2))
+    .map(pair => pair.map(toRange))
+    .map(([a, b]) => comparator(a, b))
     .reduce(add);
 };
 
 export const day04 = {
   part1: (input: string) => solution(input, fullOverlap),
   part2: (input: string) => solution(input, partialOverlap),
-  default: () => 'env variable part is missing',
 };
 
 process.env.part && console.log(day04[process.env.part](input));
