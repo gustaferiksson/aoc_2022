9c6463f36f71d57826b643f8cc60badbb9bc95fc
Gustaf <gustaf.eriksson@cygni.se>
2022-12-05 20:33:42 +0100

    nah

--- day05/index.ts
+++ day05/index.ts
@@ -1,58 +1,50 @@
 import { file } from 'bun';
+import './array.extension';
+
 export const input = await file('input.txt').text();
 
-type Move = { qty: number; from: number; to: number };
+type Stacks = string[][];
+type Move = { amount: number; from: number; to: number };
 
-const getTop = (matrix: string[][]) => matrix.map(array => array.slice(0, 1)).join('');
+const getTopCrates = (stacks: Stacks) => stacks.map(array => array.slice(0, 1)).join('');
 
-const transpose = (matrix: string[][]) => {
-  return matrix[0].map((_, i) => matrix.map(row => row[i]));
-};
-
-const parseInput = (input: string) => {
-  const [map, moves] = input.split('\n\n');
-
-  return {
-    crates: parseMap(map),
-    moves: moves.split('\n').map(parseMove),
-  };
-};
-
-const parseMap = (map: string) => {
-  const matrix = map
-    .replace(/\[|\]|\d/g, ' ')
+const parseMoves = (string: string): Move[] => {
+  return string
     .split('\n')
-    .map(row => [...row]);
-
-  return transpose(matrix)
-    .map(x => x.filter(y => y != ' '))
-    .filter(x => x.length);
+    .map(line => line.split(' '))
+    .map(splits => {
+      return { amount: +splits[1], from: +splits[3] - 1, to: +splits[5] - 1 };
+    });
 };
 
-const parseMove = (move: string): Move => {
-  const splits = move.split(' ');
-  return {
-    qty: +splits[1],
-    from: +splits[3] - 1,
-    to: +splits[5] - 1,
-  };
+const parseStacks = (string: string): Stacks => {
+  return string
+    .split('\n')
+    .map(row => [...row])
+    .transpose()
+    .map(row => row.join('').replace(/[\[\]\d\s]/g, ''))
+    .filter(x => x.length)
+    .map(stack => [...stack]);
 };
 
-const solution = (input: string, reverse = true): string => {
-  const { crates, moves } = parseInput(input);
+const crateMover = (input: string, version = 9000): string => {
+  const [stacks_raw, moves_raw] = input.split('\n\n');
 
-  moves.map(({ qty, from, to }) => {
-    const moveCrates = crates[from].splice(0, qty);
-    if (reverse) moveCrates.reverse();
-    crates[to].unshift(...moveCrates);
+  const stacks = parseStacks(stacks_raw);
+  const moves = parseMoves(moves_raw);
+
+  moves.forEach(({ amount, from, to }) => {
+    const crates = stacks[from].splice(0, amount);
+    if (version === 9000) crates.reverse();
+    stacks[to].unshift(...crates);
   });
 
-  return getTop(crates);
+  return getTopCrates(stacks);
 };
 
 export const day05 = {
-  part1: (input: string) => solution(input),
-  part2: (input: string) => solution(input, false),
+  part1: (input: string) => crateMover(input, 9000),
+  part2: (input: string) => crateMover(input, 9001),
 };
 
 process.env.part && console.log(day05[process.env.part](input));
