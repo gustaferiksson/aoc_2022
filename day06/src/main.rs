0a66d42789438b951e9ebd66edc41bb29a11d2b2
Gustaf <gustaf.eriksson@cygni.se>
2022-12-07 15:46:34 +0100

    added tests

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::env;
 use std::fs;
 
-fn marker(input: &str, count: usize) -> usize {
+fn find_marker(input: &str, count: usize) -> usize {
     let index = input
         .chars()
         .collect::<Vec<char>>()
@@ -22,8 +22,35 @@ fn main() {
     let part = env::var("part").unwrap_or("part1".to_string());
 
     match part.as_str() {
-        "part1" => println!("{}", marker(&input, 4)),
-        "part2" => println!("{}", marker(&input, 14)),
+        "part1" => println!("{}", find_marker(&input, 4)),
+        "part2" => println!("{}", find_marker(&input, 14)),
         _ => unimplemented!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE1: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    const EXAMPLE2: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    const EXAMPLE3: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+    const EXAMPLE4: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    const EXAMPLE5: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(find_marker(&EXAMPLE1, 4), 7);
+        assert_eq!(find_marker(&EXAMPLE2, 4), 5);
+        assert_eq!(find_marker(&EXAMPLE3, 4), 6);
+        assert_eq!(find_marker(&EXAMPLE4, 4), 10);
+        assert_eq!(find_marker(&EXAMPLE5, 4), 11);
+    }
+    #[test]
+    fn part2_example() {
+        assert_eq!(find_marker(&EXAMPLE1, 14), 19);
+        assert_eq!(find_marker(&EXAMPLE2, 14), 23);
+        assert_eq!(find_marker(&EXAMPLE3, 14), 23);
+        assert_eq!(find_marker(&EXAMPLE4, 14), 29);
+        assert_eq!(find_marker(&EXAMPLE5, 14), 26);
     }
 }
