8c75bef2f6f86d3903941933e84b01550d00523f
Gustaf <gustaf.eriksson@cygni.se>
2022-12-10 17:07:06 +0100

    📺 📺 📺

--- day10/index.ts
+++ day10/index.ts
@@ -1,12 +1,14 @@
 import { file } from 'bun';
 export const input = await file('input.txt').text();
 
+type Cycles = number[];
+type Crt = ('#' | ' ')[];
 enum Op {
   noop = 'noop',
   addx = 'addx',
 }
 
-const part1 = (input: string) => {
+const get_cycles = (input: string): Cycles => {
   let v = 1;
   const cycles = [];
 
@@ -20,39 +22,31 @@ const part1 = (input: string) => {
       v += +value;
     }
   }
+  return cycles;
+};
+
+const get_crt = (cycles: Cycles): Crt => {
+  return cycles.map((value, i) => {
+    const sprite = i % 40;
+    return sprite >= value - 1 && sprite <= value + 1 ? '#' : ' ';
+  });
+};
+
+const crt_to_image = (crt: Crt): string => {
+  return crt.reduce((acc, curr, i) => (i % 40 === 0 ? `${acc}\n${curr}` : `${acc}${curr}`), '');
+};
+
+const part1 = (input: string): number => {
+  const cycles = get_cycles(input);
 
   return [20, 60, 100, 140, 180, 220].reduce((acc, curr) => acc + curr * cycles[curr - 1], 0);
 };
 
-const part2 = (input: string) => {
-  let v = 1;
-  const cycles = [];
-  const crt = [];
+const part2 = (input: string): string => {
+  const cycles = get_cycles(input);
+  const crt = get_crt(cycles);
 
-  for (const line of input.split('\n')) {
-    const [op, value] = line.split(' ');
-
-    if (op == Op.noop) cycles.push(v);
-    if (op == Op.addx) {
-      cycles.push(v);
-      cycles.push(v);
-      v += +value;
-    }
-  }
-
-  for (const [i, value] of cycles.entries()) {
-    const sprite = i % 40;
-    if (sprite >= value - 1 && sprite <= value + 1) crt.push('#');
-    else crt.push(' ');
-  }
-
-  let row = '';
-  for (const [i, pixel] of crt.entries()) {
-    if (i % 40 === 0) row = `${row}\n`;
-    row = `${row}${pixel}`;
-  }
-  return row;
-  // console.log(crt);
+  return crt_to_image(crt);
 };
 
 export const day10 = {
